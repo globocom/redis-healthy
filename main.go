6f434a970a2c653565f2eee5c4fe388bf457069a
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2016-11-25 15:51:16 -0200

    moves method near to its usage

--- main.go
+++ main.go
@@ -34,6 +34,58 @@ func main() {
 	log.Println("finished")
 }
 
+func getConfig() (configuration, error) {
+	var err error
+	config := configuration{}
+
+	config.redisHost, err = fetchMandatoryParameter("REDIS_HOST")
+	if err != nil {
+		return config, err
+	}
+
+	config.logstashHost, err = fetchMandatoryParameter("LOGSTASH_HOST")
+	if err != nil {
+		return config, err
+	}
+
+	config.logstashPort, err = fetchMandatoryParameter("LOGSTASH_PORT")
+	if err != nil {
+		return config, err
+	}
+
+	config.redisMasterName = fetchParameter("REDIS_MASTER_NAME")
+	config.redisPwd = fetchParameter("REDIS_PWD")
+	config.redisSentinel = fetchParameter("REDIS_SENTINEL")
+
+	if config.redisSentinel != "" && config.redisMasterName == "" {
+		return config, errors.New("When you're using sentinel you must provide the env REDIS_MASTER_NAME")
+	}
+
+	config.redisLatencyThreshold = fetchParameter("REDIS_LATENCY_THRESHOLD")
+
+	// a list of fields one want to measure separated by , ex: "client_longest_output_list,connected_clients"
+	redisMetricsToWatch := fetchParameter("REDIS_METRICS_TO_WATCH")
+
+	if redisMetricsToWatch == "" {
+		redisMetricsToWatch = "client_longest_output_list,connected_clients,blocked_clients,rejected_connections,instantaneous_input_kbps,instantaneous_output_kbps,instantaneous_ops_per_sec,keyspace_hits,keyspace_misses,mem_fragmentation_ratio,sync_full,sync_partial_ok,sync_partial_err"
+	}
+
+	config.pingFrequency, _ = strconv.Atoi(fetchParameter("PING_FREQUENCY"))
+
+	if config.pingFrequency == 0 {
+		config.pingFrequency = 10
+	}
+
+	config.redisListMetricsToWatch = strings.Split(redisMetricsToWatch, ",")
+
+	config.project, err = fetchMandatoryParameter("PROJECT")
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
 func ping(config configuration) error {
 	log.Println("starting ping")
 
@@ -87,58 +139,6 @@ type configuration struct {
 	redisListMetricsToWatch []string
 	pingFrequency           int
 	project                 string
-}
-
-func getConfig() (configuration, error) {
-	var err error
-	config := configuration{}
-
-	config.redisHost, err = fetchMandatoryParameter("REDIS_HOST")
-	if err != nil {
-		return config, err
-	}
-
-	config.logstashHost, err = fetchMandatoryParameter("LOGSTASH_HOST")
-	if err != nil {
-		return config, err
-	}
-
-	config.logstashPort, err = fetchMandatoryParameter("LOGSTASH_PORT")
-	if err != nil {
-		return config, err
-	}
-
-	config.redisMasterName = fetchParameter("REDIS_MASTER_NAME")
-	config.redisPwd = fetchParameter("REDIS_PWD")
-	config.redisSentinel = fetchParameter("REDIS_SENTINEL")
-
-	if config.redisSentinel != "" && config.redisMasterName == "" {
-		return config, errors.New("When you're using sentinel you must provide the env REDIS_MASTER_NAME")
-	}
-
-	config.redisLatencyThreshold = fetchParameter("REDIS_LATENCY_THRESHOLD")
-
-	// a list of fields one want to measure separated by , ex: "client_longest_output_list,connected_clients"
-	redisMetricsToWatch := fetchParameter("REDIS_METRICS_TO_WATCH")
-
-	if redisMetricsToWatch == "" {
-		redisMetricsToWatch = "client_longest_output_list,connected_clients,blocked_clients,rejected_connections,instantaneous_input_kbps,instantaneous_output_kbps,instantaneous_ops_per_sec,keyspace_hits,keyspace_misses,mem_fragmentation_ratio,sync_full,sync_partial_ok,sync_partial_err"
-	}
-
-	config.pingFrequency, _ = strconv.Atoi(fetchParameter("PING_FREQUENCY"))
-
-	if config.pingFrequency == 0 {
-		config.pingFrequency = 10
-	}
-
-	config.redisListMetricsToWatch = strings.Split(redisMetricsToWatch, ",")
-
-	config.project, err = fetchMandatoryParameter("PROJECT")
-	if err != nil {
-		return config, err
-	}
-
-	return config, nil
 }
 
 func fetchMandatoryParameter(key string) (string, error) {
